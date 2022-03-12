84a2156c5f969fb6e9d54d22cbd218e6cecab46d
RinalisDS <denispilyutin@gmail.com>
2022-03-12 17:42:08 +0200

    added tasks request for todolist api

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -22,15 +22,21 @@ export const todolistApi = {
     },
     updateTodoTitle(title: string, todolistID: string) {
         return instance.put<BaseResponseType>(`todo-lists/${todolistID}`, {title})
+    },
+    getTasks(todolistID: string) {
+        return instance.get<GetTasksResponseType>(`todo-lists/${todolistID}/tasks`)
+    },
+    deleteTask(todolistID: string, taskID: string) {
+        return instance.delete<BaseResponseType>(`todo-lists/${todolistID}/tasks/${taskID}`)
+    },
+    updateTask(todolistID: string, taskID: string, model: UpdateTaskModelType) {
+        return instance.put<BaseResponseType>(`todo-lists/${todolistID}/tasks/${taskID}` , model)
+    },
+    createTask(todolistID: string, taskID: string, title: string) {
+        return instance.post<BaseResponseType<{item: TaskType}>>(`todo-lists/${todolistID}/tasks` , {title})
     }
 }
 
-export type TodolistType = {
-    id: string
-    title: string
-    addedDate: string
-    order: number
-}
 
 type BaseResponseType<T = {}> = {
     resultCode: number
@@ -40,25 +46,27 @@ type BaseResponseType<T = {}> = {
 
 }
 
+type GetTasksResponseType = {
+    items: Array<TaskType>
+    totalCount: number
+    error: string | null
+}
 
 
-type GetTasksResponseType =  { items: Array<TaskType> , totalCount: number, error: string}
-
-export enum TaskStatuses  {
+export enum TaskStatuses {
     New = 0,
     InProgress = 1,
     Completed = 2,
-    Draft = 3 ,
+    Draft = 3,
 }
 
 export enum TaskPriorities {
-    Low = 0 ,
-    Middle = 1 ,
+    Low = 0,
+    Middle = 1,
     High = 2,
     Urgently = 3,
-    Later= 4 ,
+    Later = 4,
 }
-
 
 
 export type TaskType = {
@@ -77,9 +85,15 @@ export type TaskType = {
 type UpdateTaskModelType = {
     title: string
     description: string
-    // completed: boolean
     status: number
     priority: number
     startDate: string
     deadline: string
+}
+
+export type TodolistType = {
+    id: string
+    title: string
+    addedDate: string
+    order: number
 }
