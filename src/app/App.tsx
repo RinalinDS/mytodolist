6ae8934b3d3e18107ff8e55983d3cc5ce634c14b
RinalisDS <denispilyutin@gmail.com>
2022-04-01 15:01:18 +0300

    fixed logout mistake , when  state wasn't cleared after

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,10 @@
 import React, {useEffect} from 'react';
 import './App.css';
-import Typography from "@material-ui/core/Typography";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
-import IconButton from "@material-ui/core/IconButton";
 import Toolbar from "@material-ui/core/Toolbar";
-import Menu from "@mui/icons-material/Menu";
 import CircularProgress from '@mui/material/CircularProgress';
 import {TodolistsList} from "../features/TodolistList/TodolistsList";
 import {initializeAppTC, RequestStatusType} from "./AppReducer";
@@ -20,14 +17,13 @@ import {useDispatch} from 'react-redux';
 import {logoutTC} from '../features/Login/authReducer';
 
 
-
 export function App() {
     const status = useAppSelector<RequestStatusType>(state => state.app.status)
     const isInitialized = useAppSelector<boolean>(state => state.app.isInitialized)
     const isLoggedIn = useAppSelector<boolean>(state => state.auth.isLoggedIn)
     const dispatch = useDispatch()
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(initializeAppTC())
     }, [])
 
@@ -64,10 +60,11 @@ export function App() {
             <Container fixed>
 
                 <Routes>
-                    <Route path={'login'} element={<Login/>}/>
                     <Route path={'/'} element={<TodolistsList/>}/>
-                    <Route path={'404'} element={<h1>Someone FUCKED UP</h1>} />
-                    <Route path={'*'} element={<Navigate to={'404'} />} />
+                    <Route path={'login'} element={<Login/>}/>
+
+                    <Route path={'404'} element={<h1>Someone FUCKED UP</h1>}/>
+                    <Route path={'*'} element={<Navigate to={'404'}/>}/>
 
                 </Routes>
             </Container>
